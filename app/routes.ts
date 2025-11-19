7be41d2e870bce81503cf393cdfa2bd8cf35129d
RaynerCuanda <raynercuandas@gmail.com>
2025-11-19 16:44:36 +0700

    forgot password page

--- app/routes.ts
+++ app/routes.ts
@@ -2,5 +2,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"),
-    route("login", "routes/login.tsx")
+    route("login", "routes/login.tsx"),
+    route("forgot_password", "routes/forgot_password.tsx")
 ] satisfies RouteConfig;
+ 
